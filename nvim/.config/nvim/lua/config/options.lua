9c0770ec85104ef8cbb2eab600a23d2debb55f1b
Nicolas Scotto Di Perto <nicolas.scottodiperto@gmail.com>
2026-04-03 10:17:22 +0100

    [nvim] wsl yank

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -7,14 +7,38 @@ opt.colorcolumn = "80"
 opt.swapfile = false
 
 vim.g.clipboard = {
-  name = "win32yank-wsl",
-  copy = {
-    ["+"] = "win32yank.exe -i --crlf",
-    ["*"] = "win32yank.exe -i --crlf",
-  },
-  paste = {
-    ["+"] = "win32yank.exe -o --lf",
-    ["*"] = "win32yank.exe -o --lf",
-  },
-  cache_enabled = 0,
+	name = "win32yank-wsl",
+	copy = {
+		["+"] = "win32yank.exe -i --crlf",
+		["*"] = "win32yank.exe -i --crlf",
+	},
+	paste = {
+		["+"] = "win32yank.exe -o --lf",
+		["*"] = "win32yank.exe -o --lf",
+	},
+	cache_enabled = 0,
 }
+
+-- local function paste()
+-- 	local content = vim.fn.system("win32yank.exe -o --lf")
+-- 	return { vim.split(content, "\n"), "v" }
+-- end
+--
+-- local function copy(lines, _)
+-- 	local content = table.concat(lines, "\n")
+-- 	vim.fn.system("win32yank.exe -i --crlf", content)
+-- 	-- Also use OSC 52 as backup
+-- 	require("vim.ui.clipboard.osc52").copy("+")(lines)
+-- end
+--
+-- vim.g.clipboard = {
+-- 	name = "win32yank-wsl",
+-- 	copy = {
+-- 		["+"] = copy,
+-- 		["*"] = copy,
+-- 	},
+-- 	paste = {
+-- 		["+"] = paste,
+-- 		["*"] = paste,
+-- 	},
+-- }
